de468a69e1ec52292ac65468a6bd3738011e5106
carl-mate-sun-asterisk <carljoseph.mate@sun-asterisk.com>
2022-08-09 11:19:24 +0800

    Adjust frontend to apply HTML tags from django-summernote

--- backend/blog_backend/urls.py
+++ backend/blog_backend/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework import routers
 from post import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'posts', views.PostView, 'post')
@@ -31,3 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
