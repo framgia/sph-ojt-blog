ef105d5c2213a385a2f2c9df4c8a18d602ca3120
Zen Velez <108708365+velezrenz@users.noreply.github.com>
2022-08-10 16:31:39 +0800

    Blog/blog detail frontend development (#3)
    * Blog Detail Frontend Development
    
    * Blog Detail Backend Development (#2)
    
    * added blog details
    
    * Update views.py
    
    * Update views.py
    
    * Update models.py
    
    * Add pycache in git ignore
    
    * deleted migrations' pycache
    
    * Update views.py
    
    * update changed to hooks
    
    * update onclick on title will navigate blogdetail
    
    * Blog Detail Frontend Development
    
    * update changed to hooks
    
    * update added route onclick on title
    
    * added new line
    
    Co-authored-by: Russell Sean Gonzalve <108708225+russellseanrs@users.noreply.github.com>

---
+++ frontend/src/Navbar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NavBar.css';
+const NavBar = () => {
+	const navigate = useNavigate();
+	return (
+		<>
+			<div className='ui fluid secondary pointing menu'>
+				<div className='item active' onClick={() => navigate('/')}>
+					Home
+				</div>
+				<div className='item'>All Blogs</div>
+			</div>
+
+			<div className='ui hidden divider'></div>
+		</>
+	);
+};
+
+export default NavBar;
