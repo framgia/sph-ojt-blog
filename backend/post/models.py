da4c5ec3cf69d16df462fa8891aa42c878b02c03
carl-mate-sun-asterisk <carljoseph.mate@sun-asterisk.com>
2022-08-10 16:40:31 +0800

    Migrate to database part 2

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,24 +1,56 @@
 from django.db import models
+# Add this back when the Login/Signup function is complete
+#from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
+class DummyUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email')
 
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=255, null=True, blank=True)
-    slug = models.SlugField()
-    content = models.TextField(null=True, blank=True)
-    excerpt = models.CharField(max_length=255, null=True, blank=True)
-    author = models.CharField(max_length=30, null=False, blank=True)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Comment(models.Model):
+    author_fk = models.ForeignKey(
+            DummyUser, on_delete=models.CASCADE, related_name='comment_author', default=None)
+    content = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
-    total_votes = models.IntegerField(null=True, blank=True, default=0)
-    
+
     class Meta:
-        ordering = ['-published_at']
+        ordering = ('-published_at',)
+
+class Vote(models.Model):
+    author_fk = models.ForeignKey(
+            DummyUser, on_delete=models.CASCADE, related_name='voter', default=None)
+    total_votes = models.IntegerField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    content = models.TextField()
+    excerpt = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    published_at = models.DateTimeField(auto_now_add=True)
+    comment_fk = models.ManyToManyField(
+                Comment, blank=True)
+    vote_fk = models.ManyToManyField(
+            Vote, blank=True)
+
+    # This refers to the comment
+    @property
+    def comment_content(self):
+        return self.comment_fk.content
+
+    # This refers to the upvote
+    @property
+    def no_of_upvotes(self):
+        return self.vote_fk.total_votes
 
     def save(self, *args, **kwargs):
         original_slug = slugify(self.title)
         queryset = Post.objects.all().filter(slug__iexact=original_slug).count()
-        
+
         # Ensures that the slug is unique
         count = 1
         slug = original_slug
@@ -32,4 +64,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
