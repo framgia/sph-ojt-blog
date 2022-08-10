7c20f74c6b0d8141ca60c097051a8b2cb326661f
Zen Velez <renzbrian.velez@sun-asterisk.com>
2022-08-10 15:09:16 +0800

    update onclick on title will navigate blogdetail

--- frontend/src/HomePage/HomePage.js
+++ frontend/src/HomePage/HomePage.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import API from '../API';
 import NavBar from '../Navbar/NavBar';
+import { useNavigate } from 'react-router-dom';
 
-class HomePage extends React.Component {
-	state = { details: [] };
+const HomePage = () => {
+	const [posts, setPosts] = useState([]);
+	const navigate = useNavigate();
 
-	componentDidMount() {
-		//let data;
-		axios
-			.get('http://localhost:8000/api/posts/')
-			.then((res) => this.setState({ details: res.data }))
+	useEffect(() => {
+		API.get('/posts/')
+			.then((res) => setPosts(res.data))
 			.catch((err) => console.log(err));
-	}
-	render() {
-		return (
-			<>
-				<div>
-					<NavBar />
-				</div>
-				<div>
-					<header>(Sunposterisk) Data Generated From Django</header>
-					<hr></hr>
-					{this.state.details.map((output, id) => (
-						<div key={id}>
-							<div>
-								<h1>Title: {output.title}</h1>
-								<p>Content: {output.content}</p>
-								<p>Author: {output.author}</p>
-								<p>Published: {output.published_at}</p>
-								<p>Votes: {output.total_votes}</p>
-							</div>
+	}, []);
+
+	return (
+		<>
+			<div>
+				<NavBar />
+			</div>
+			<div>
+				<header>(Sunposterisk) Data Generated From Django</header>
+				<hr></hr>
+				{posts?.map((output, id) => (
+					<div key={id}>
+						<div>
+							<h1 onClick={() => navigate('/blogs/:id')}>
+								Title: {output.title}
+							</h1>
+							<p>Content: {output.content}</p>
+							<p>Author: {output.author}</p>
+							<p>Published: {output.published_at}</p>
+							<p>Votes: {output.total_votes}</p>
 						</div>
-					))}
-				</div>
-			</>
-		);
-	}
-}
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
 
 export default HomePage;
