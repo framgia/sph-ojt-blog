a728e139b113ec4e34ffbe2f924b501d3a3b3847
Russell Sean Gonzalve <108708225+russellseanrs@users.noreply.github.com>
2022-08-10 13:26:29 +0800

    Blog Detail Backend Development (#2)
    * added blog details
    
    * Update views.py
    
    * Update views.py
    
    * Update models.py
    
    * Add pycache in git ignore
    
    * deleted migrations' pycache
    
    * Update views.py

--- backend/post/models.py
+++ backend/post/models.py
@@ -4,13 +4,16 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255, null=True, blank=True)
     slug = models.SlugField()
-    content = models.TextField()
-    excerpt = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    excerpt = models.CharField(max_length=255, null=True, blank=True)
+    author = models.CharField(max_length=30, null=False, blank=True)
     published_at = models.DateTimeField(auto_now_add=True)
-    total_votes = models.IntegerField()
+    total_votes = models.IntegerField(null=True, blank=True, default=0)
+    
+    class Meta:
+        ordering = ['-published_at']
 
     def save(self, *args, **kwargs):
         original_slug = slugify(self.title)
--- backend/post/views.py
+++ backend/post/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .serializers import PostSerializer
 from .models import Post
 
 
 # Create your views here.
 class PostView(viewsets.ModelViewSet):
-    queryset = Post.objects.order_by('-published_at')
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'slug'
 
 class PostDetailView(viewsets.ModelViewSet):
-    queryset = Post.objects.order_by('-published_at')
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     lookup_field = 'slug'
 
@@ -20,4 +21,11 @@ class PostDetailView(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], author=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError("This is not your post to delete")
 
