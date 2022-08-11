ce3aab62d78cd2ac3a31314485229e269049670d
carl-mate-sun-asterisk <108708114+carl-mate-sun-asterisk@users.noreply.github.com>
2022-08-11 14:05:45 +0800

    Update backend/post/pagination.py
    Co-authored-by: chevyB <chevbanico@gmail.com>

--- backend/post/pagination.py
+++ backend/post/pagination.py
@@ -4,4 +4,4 @@ class CustomPageNumberPagination(pagination.PageNumberPagination):
     page_size = 1
     page_size_query_param = 'count'
     max_page_size = 5
-    page_query_param = 'p'
+    page_query_param = 'page'
