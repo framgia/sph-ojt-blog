8e1d8a89df2f6a49698b82461aac4b78695ffd38
jhoannaricaa <jhoannarica.loberanes@sun-asterisk.com>
2022-08-11 18:18:56 +0800

    Pre frontend of blog Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import BlogDetail from './BlogDetail/BlogDetail';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage/HomePage';
+import Home from './Home/Home';
 class App extends React.Component {
 	render() {
 		return (
 			<Routes>
-				<Route path='/' element={<HomePage />} />
+				<Route path='/' element={<Home />} />
 				<Route path='/blogs/:id' element={<BlogDetail />} />
 			</Routes>
 		);
