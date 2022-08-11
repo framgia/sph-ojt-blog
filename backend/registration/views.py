6817b460f76650c8860172db2d84a94346e861a6
ahyieka <joejeaika.blanes@sun-asterisk.com>
2022-08-11 14:16:16 +0800

    Added def get in views.py

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -6,10 +6,14 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
+from django.contrib.auth.models import User
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
+
+    permission_classes = (permissions.AllowAny,)
+    http_method_names = ['get', 'head', 'post']
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -19,3 +23,10 @@ class RegisterAPI(generics.GenericAPIView):
         "user": UserSerializer(user, context=self.get_serializer_context()).data,
         "token": AuthToken.objects.create(user)[1]
         })
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = RegisterSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    queryset = User.objects.all()
