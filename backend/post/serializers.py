6b00fdf575bd51b3c0838e9918f96c663b57205e
Russell Sean Gonzalve <108708225+russellseanrs@users.noreply.github.com>
2022-08-15 15:07:32 +0800

    New Fixed "Refactor models.py and fix comment bug" (#17)
    * new branch
    
    * fixed posts

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment, DummyUser
+
+class DummyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DummyUser
+        fields = '__all__'
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
         lookup_field = 'slug'
-        
+
+class CommentSerializer(serializers.ModelSerializer):
+    post = PostSerializer(read_only=True)
+    post_id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
