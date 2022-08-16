7d9d8d618cf2136c1b293faed2be6717cbe55b43
ahyieka <joejeaika.blanes@sun-asterisk.com>
2022-08-16 10:36:50 +0800

    Removed the forward slash at the end of the URLs

--- backend/registration/urls.py
+++ backend/registration/urls.py
@@ -4,7 +4,7 @@ from knox import views as knox_views
 
 urlpatterns = [
     path('api/user/register', RegisterAPI.as_view(), name='register'),
-    path('api/user/login/', LoginAPI.as_view(), name='login'),
-    path('api/user/logout/', knox_views.LogoutView.as_view(), name='logout'),
-    path('api/user/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/user/login', LoginAPI.as_view(), name='login'),
+    path('api/user/logout', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/user/logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
